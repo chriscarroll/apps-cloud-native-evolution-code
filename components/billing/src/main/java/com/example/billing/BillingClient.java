7a3ed8d6880459dd7c6a3c8d724ca56bfca5730b
Chris Carroll <chris.carroll1@gmail.com>
2017-10-23 15:41:49 -0600

    implement circuit breaker

--- components/billing/src/main/java/com/example/billing/BillingClient.java
+++ components/billing/src/main/java/com/example/billing/BillingClient.java
@@ -1,8 +1,13 @@
 package com.example.billing;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 
 public class BillingClient {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     private final RestTemplate restTemplate;
     private final String serviceEndpoint;
@@ -12,7 +17,12 @@ public class BillingClient {
         this.serviceEndpoint = endpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "defaultBillUser")
     public void billUser(String userId, int amount) {
         restTemplate.postForEntity(serviceEndpoint + "/reoccurringPayment", amount, String.class);
+    }
+
+    public void defaultBillUser(String userId, int amount) {
+        logger.info("billUser has failed for userId [{}] with amount [{}]", userId, amount);
     }
 }
